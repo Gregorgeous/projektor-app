6dafd45ff02eb263417f07463877ef08b2058607
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2018-07-21 23:05:49 +0100

    Done: CSS animation fix. Fixed a random CSS bug that makes the horizontal rule under the h1 "flicker" (i.e. suddenly changed the boldness of the rule) Solution: make the scaleX NOT come back to 1 (e.g. transform: scaleX(1.01))

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -2,6 +2,7 @@
   <div class="landing-page">
     <!-- <unitLeaderSection/> -->
     <h1 id="lp-logo">Projektor</h1>
+    
   </div>
 </template>
 
@@ -29,23 +30,30 @@
       &::after {
         content: '';
         height: 1px;
-        opacity: .4;
         border-radius: 20px;
         width: 90%;
         right: -20px;
         position: absolute;
         background-color: black;
         bottom: -12px;
-        box-shadow: 2px 2px 6px 0px rgba(78, 25, 25, 1); 
+        box-shadow: 2px 2px 6px 0px rgba(78, 25, 25, 1);
         transition: opacity 1s ease-in-out;
-        animation: underline-appear-anim 2.3s cubic-bezier(0, 0, 0.28, 1) 0.5s both, 
+        
+        animation: underline-appear-anim 2.3s cubic-bezier(0, 0, 0.28, 1) 0.5s both,
         underline-move-around 2s cubic-bezier(0.72, 0.01, 0.37, 1.01) 4s 8 alternate both,
         underline-finale 2s cubic-bezier(0.65, 0.01, 0.26, 0.99) 18s normal forwards;
-        // animation: name duration timing-function delay iteration-count direction fill-mode;
       }
     }
-  }
 
+    & #lp-subheader{
+      margin-top: 30px;
+    }
+  } 
+  
+  
+  
+  
+  // ============ Animations ================
   @keyframes opacity-appear-effect {
     from {
       opacity: 0;
@@ -72,7 +80,6 @@
     100% {
       left: 40px;
       width: 88%;
-      opacity: .4;
     }
 
   }
@@ -82,7 +89,7 @@
       left: 40px;
       transform: scaleX(.7);
     }
-    
+
     100% {
       left: 0px;
       transform: scaleX(.7);
@@ -90,14 +97,13 @@
   }
 
   @keyframes underline-finale {
-    0%{
+    0% {
       transform: scaleX(.7);
-
+      opacity: 1;
     }
-    100%{
-      transform: scaleX(1);
-      opacity: .4;
+    100% {
+      transform: scaleX(1.01);
+      opacity: 0.4;
     }
   }
-
 </style>
