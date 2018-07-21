5b060ed41d83f2c073f30a6e1b83e936b0e27dc8
Grzegorz Rybak <g.r.fisher.pl@gmail.com>
2018-07-21 17:16:22 +0100

    Done: 1. Set-up the project structure 2. Created first element on the website - animated-on-hover scout logo Todo: make the logo 'come back' smoothly during off-hover

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <!-- <unitLeaderSection/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import unitLeaderSection from '@/components/unitLeaderSection.vue'
+
+export default {
+  name: 'home',
+  components: {
+    unitLeaderSection
+  }
+}
+</script>
